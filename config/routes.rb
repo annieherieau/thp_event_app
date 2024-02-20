20b10508ec41d058ab20c57c0a242174e75b5f77
annieherieau <annie.herieau@gmail.com>
2024-02-20 16:44:23 +0100

    Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,5 +11,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "static_pages#index"
+  root "events#index"
 end
