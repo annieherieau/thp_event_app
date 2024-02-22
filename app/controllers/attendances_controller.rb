bc9c448df71c8f21fa55f5d45e52b0098b9a0d8e
annieherieau <annie.herieau@gmail.com>
2024-02-22 19:15:21 +0100

    incription event à 0€

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,19 @@
 class AttendancesController < ApplicationController
   before_action :authenticate_user!
 
+  def create
+    @user = User.find(params['user_id'])
+    @attendance = Attendance.new(
+      user_id: @user.id,
+      event_id: params['event_id']
+    )
+    if @attendance.save
+      @user.new_attendance_send(@event_id)
+      render event_path(@event_id)
+    else
+      flash[:alert] = "Un problème est survenu, merci de contacter l'administrateur"
+    end
+  end
+
   
 end
