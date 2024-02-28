df05f73e4068592ea67eda23e8bee26cca8c1515
annieherieau <annie.herieau@gmail.com>
2024-02-28 13:13:49 +0100

    flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
 
+  def bootstrap_class_for_flash(type)
+    case type
+    when 'notice' then 'alert-info'
+    when 'success' then 'alert-success'
+    when 'error' then 'alert-denger'
+    when 'alert' then 'alert-warning'
+    else
+      'alert-info'
+    end
+  end
 end
